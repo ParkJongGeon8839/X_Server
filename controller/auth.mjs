9733de8542e487833163e28902f0a6eddb4ad256
ParkJongGeon8839 <whdrjs8839@gmail.com>
2025-11-21 12:33:20 +0900

    배열 데이터 API 테스트 - 유저 추가

---
+++ controller/auth.mjs
@@ -0,0 +1,30 @@
+import express from "express";
+import * as authRepository from "../data/auth.mjs";
+
+// 전체 회원 리스트
+
+// 회원가입
+export async function signup(req, res, next) {
+  const { userid, password, name, email } = req.body;
+  const user = await authRepository.createUser(userid, password, name, email);
+  if (user) {
+    res.status(201).json(user);
+  }
+}
+
+// 로그인
+export async function login(req, res, next) {
+  const { userid, password } = req.body;
+  const user = await authRepository.login(userid, password);
+  if (user) {
+    res.status(200).json({ message: `${userid}님 로그인 완료!!` });
+  } else {
+    res
+      .status(404)
+      .json({ message: `${userid}님 아이디 또는 비밀번호를 확인 하세요!!` });
+  }
+}
+
+// 회원정보 수정
+
+// 회원정보 삭제
--- router/auth.mjs
+++ router/auth.mjs
@@ -1,12 +1,17 @@
 import express from "express";
+import * as authController from "../controller/auth.mjs";
 
 // router 객체 만들기
 const router = express.Router();
 
 // 회원가입
+// http://127.0.0.1:9000/auth/signup
+router.post("/signup", authController.signup);
 
 // 로그인
+router.post("/login", authController.login);
 
 // 로그인 유지
+
 // router 객체를 다른곳에 사용할수 있도록
 export default router;
